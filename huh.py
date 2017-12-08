c967b6c204fd39fc83dc33ac893d5ddc005247db
poph55 <palmalama@gmail.com>
2017-12-08 12:56:44 -0500

    getting there

--- huh.py
+++ huh.py
@@ -12,8 +12,56 @@ class PigeonMessage:
 				array[x] = '1'
 			elif array[x].lower() == 'b':
 				array[x] = '2'
+			elif array[x].lower() == 'c':
+				array[x] = '3'
+			elif array[x].lower() == 'd':
+				array[x] = '4'
+			elif array[x].lower() == 'e':
+				array[x] = '5'
+			elif array[x].lower() == 'f':
+				array[x] = '6'
+			elif array[x].lower() == 'g':
+				array[x] = '7'
+			elif array[x].lower() == 'h':
+				array[x] = '8'
+			elif array[x].lower() == 'i':
+				array[x] = '9'
+			elif array[x].lower() == 'j':
+				array[x] = '10'
+			elif array[x].lower() == 'k':
+				array[x] = '11'
+			elif array[x].lower() == 'l':
+				array[x] = '12'
+			elif array[x].lower() == 'm':
+				array[x] = '13'
+			elif array[x].lower() == 'n':
+				array[x] = '14'
+			elif array[x].lower() == 'o':
+				array[x] = '15'
+			elif array[x].lower() == 'p':
+				array[x] = '16'
+			elif array[x].lower() == 'q':
+				array[x] = '17'
+			elif array[x].lower() == 'r':
+				array[x] = '18'
+			elif array[x].lower() == 's':
+				array[x] = '19'
+			elif array[x].lower() == 't':
+				array[x] = '20'
+			elif array[x].lower() == 'u':
+				array[x] = '21'
+			elif array[x].lower() == 'v':
+				array[x] = '22'
+			elif array[x].lower() == 'w':
+				array[x] = '23'
+			elif array[x].lower() == 'x':
+				array[x] = '24'
+			elif array[x].lower() == 'y':
+				array[x] = '25'
+			elif array[x].lower() == 'z':
+				array[x] = '26'
 			x += 1
-		message = ''.join(array)
+		self.message = array
 
 message = str(input("Enter your message here:\n"))
 message1 = PigeonMessage(message)
@@ -24,5 +72,6 @@ print(message)
 message1.secret()
 
 print(message)
+
 
 
