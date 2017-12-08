7eccc3c26574f3b58634b96dab0ae14b6bab143a
Jburt4 <jburt44@gmail.com>
2017-12-08 13:49:53 -0500

    stuff

--- runnerclass.py
+++ runnerclass.py
@@ -1,5 +1,5 @@
 #Runner Class
-import AlphabetShift
+from AlphabetShift import Encoding
 
 answer = str(input('Would you like to encode a message or decode a message? 1 for encode or 2 for decode  '))
 while answer != '1' and answer != '2':
@@ -9,8 +9,8 @@ while answer != '1' and answer != '2':
 if answer == '1':
 	code1 = Encoding(str(input('What message would you like to encode?  ')))
 	code1.encode()
-	print(string1)
-if answer =='2':
+	print(code1.string1)
+elif answer =='2':
 	code1 = Encoding(str(input('What message would you like to decode?  ')))
 	code1.decode()
-	print(string2)
+	print(code1.string2)
