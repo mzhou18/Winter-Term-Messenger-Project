3b044d5b2c06b75e6d932dac93a1c663248265a2
Jburt4 <jburt44@gmail.com>
2017-12-08 13:27:42 -0500

    rename

--- runnerclass.py
+++ runnerclass.py
@@ -1,12 +1,16 @@
 #Runner Class
+import AlphabetShift
 
-def main():
+answer = str(input('Would you like to encode a message or decode a message? 1 for encode or 2 for decode  '))
+while answer != '1' and answer != '2':
+	print('That is not an option.\n')
+	answer = str(input('Would you like to encode a message or decode a message? 1 for encode or 2 for decode  '))
 
-    message = input('MESSAGE: ')
-
-    for character in message:
-    	#converts all characters to uppercasae letters
-        print morsecode[character.upper()],
-
-if __name__ == "__main__":
-    main()
+if answer == '1':
+	code1 = Encoding(str(input('What message would you like to encode?  ')))
+	code1.encode()
+	print(string1)
+if answer =='2':
+	code1 = Encoding(str(input('What message would you like to decode?  ')))
+	code1.decode()
+	print(string2)
